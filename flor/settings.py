60fd3148154810a78379209c75fd1e6e8c9ae0bc
gabtorre <gtorresx3@gmail.com>
2020-11-11 22:07:46 -0800

    Deployment

--- flor/settings.py
+++ flor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k3^g@#3f6pz!w!-@8q5^i3)=-%bpajb*p@e03*h(=0jb*@v-_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
